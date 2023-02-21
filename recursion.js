f085ab4c73cb61c9ba81563e1a8ae066aa2e25d2
shnorlaxzz <mshnas56@gmail.com>
2023-02-21 00:36:24 +0000

    recursion exercise

--- recursion.js
+++ recursion.js
@@ -2,8 +2,12 @@
 
 function division(number, dividedBy) {
     // Write you logic here.
-    return;
+    if ((dividedBy === 0)||(number < dividedBy)) {
+        return 0;
+    }
+    return 1 + division(number - dividedBy, dividedBy);
 }
+
 
 /* Write a function that implement Math.pow(x,n) but using recursion
 Example:
@@ -13,7 +17,9 @@ pow(2,4) = 16
 
 function pow(x, n) {
     // Write you logic here.
-    return;
+    if (n==0)
+    return 1;
+    return x*pow(x,n-1);
 }
 
 /* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
@@ -27,8 +33,14 @@ Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
 
 function fibonacci(n) {
     // Write you logic here.
-    return;
-}
+    if (n === 0) {
+        return 0;
+      } else if (n === 1) {
+        return 1;
+      } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+      }
+    }
 
 /* Optional 
 
